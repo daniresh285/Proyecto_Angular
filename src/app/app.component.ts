9b97d64a2d14177f6e681a8de081249764b0e324
Daniel <danieldelosreyesruiz431@gmail.com>
2025-05-13 09:22:06 +0200

    Segundo commit haciendo cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,28 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser'; 
 import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 import { SegundoComponenteComponent } from "./segundo-componente/segundo-componente.component";
+import { FormsModule } from '@angular/forms'; //Esto se usa para usar el ngModel en el html
 
+// El decorador @Component es una función que toma un objeto de configuración
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MiComponenteComponent, SegundoComponenteComponent],
+  imports: [MiComponenteComponent, SegundoComponenteComponent, FormsModule], // Esto se usa para importar el componente hijo, que se importa
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+// La clase AppComponent es la clase principal del componente, que contiene la parte visible de la aplicación a la página web
 export class AppComponent {
   title = 'Proyecto con angular';
   nombre:string = "Daniel De Los Reyes"
+
+  number: number = 0;
+
+  // El método addOne incrementa el valor de la propiedad number en 1
+  addOne() {
+    this.number++;
+  }
+
+  // La condicion es una propiedad booleana que se inicializa en false, que hace que el componente hijo no se muestre
+  condition: boolean = false;
 }
